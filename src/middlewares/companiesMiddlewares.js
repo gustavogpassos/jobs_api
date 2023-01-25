8f0d5f48991db8df402631e44a1872945a846e57
Gustavo G Passos <girardi_gustavo@outlook.com>
2023-01-24 23:41:25 -0300

    rename file

---
+++ src/middlewares/companiesMiddlewares.js
@@ -0,0 +1,8 @@
+const companyEntity = require('../models/companiesModel')
+
+exports.alreadyExists = async (newCompany) => {
+    const company = await companyEntity.find({
+        $or: [{ cnpj: newCompany.cnpj }, { email: newCompany.email }],
+    })
+    return company
+}
